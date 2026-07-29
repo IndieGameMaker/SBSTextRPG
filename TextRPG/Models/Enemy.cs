1e06b7588f53c26cbbb6d51cc9283940d6de64fe
Lee JaeHyun <myleje@gmail.com>
2026-07-29 20:32:29 +0900

    feat: 적 생성 메서드에 난이도 설정 로직 추가 및 정보 출력 메서드 구현

--- TextRPG/Models/Enemy.cs
+++ TextRPG/Models/Enemy.cs
@@ -15,7 +15,7 @@ public class Enemy : Character
     #endregion
 
     #region 메서드
-
+    // 적 생성 메서드 (레벨에 따라서 난이도 설정)
     // 변수 : 카멜케이스 camelCase
     public static Enemy CreateEnemy(int playerLevel)
     {
@@ -23,7 +23,8 @@ public class Enemy : Character
         Random random = new Random();
         // 적 캐릭터의 레벨 (플레이어 레벨 +-1) -1, 0, +1
         int enemyLevel = Math.Max(1, playerLevel + random.Next(-1, 2)); // -1, 0, +1
-        // 적 캐릭터의 종류
+        // 적 캐릭터의 종류 
+        // 형변환 => (변환할타입)변수
         EnemyType enemyType = (EnemyType)random.Next(0, 3); // 0, 1, 2
         
         // 적 캐릭터 스텟
@@ -32,6 +33,15 @@ public class Enemy : Character
         int attackPower = 10 + (enemyLevel - 1) * 5;
         int defense = 5 + (enemyLevel - 1) * 3;
         int goldReward = 20 + (enemyLevel - 1) * 10;
+
+        return new Enemy($"LV{enemyLevel} {enemyType.ToString()}", maxHp, maxMp, attackPower, defense, enemyLevel, goldReward);
+    }
+    
+    // 적 캐릭터 정보 출력
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"리워드: {GoldReward}");
     }
 
     #endregion
