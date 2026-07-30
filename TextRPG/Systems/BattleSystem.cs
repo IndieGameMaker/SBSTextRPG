f118e244a69450f7bc7e6d8528d9bad53cc11382
Lee JaeHyun <myleje@gmail.com>
2026-07-30 20:33:55 +0900

    feat: 플레이어 턴 메서드에서 도망 기능 추가

--- TextRPG/Systems/BattleSystem.cs
+++ TextRPG/Systems/BattleSystem.cs
@@ -19,11 +19,16 @@ public class BattleSystem
         int turn = 1;
         
         // 전투로직 실행
-        // while (player.IsAlive && enemy.IsAlive)
+        while (player.IsAlive && enemy.IsAlive)
         {
             Console.WriteLine($"\n======= {turn} 턴 =======");
             // 플레이어 턴 실행
-            PlayerTurn(player, enemy);
+            if (!PlayerTurn(player, enemy))
+            {
+               // 플레이어 도망친 경우
+               Console.WriteLine("\n전투에서 도망쳤습니다....");
+               return false;
+            }
             // 적 캐릭터 사망 여부 판단
             // 적 공격 턴
             turn++;
