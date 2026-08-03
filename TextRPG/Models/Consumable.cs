34fe2376bdded5f6a54ba312f65d1aa7062cfc33
Lee JaeHyun <myleje@gmail.com>
2026-08-03 20:27:05 +0900

    feat: enhance Consumable class to include HP and MP recovery logic

--- TextRPG/Models/Consumable.cs
+++ TextRPG/Models/Consumable.cs
@@ -28,6 +28,35 @@ public class Consumable : Item
     {
         // 플레이어의 HP/MP를 회복하는 로직
         bool isUsed = false;
+
+        if (HpAmount > 0)
+        {
+            int healedHp = player.HealHp(HpAmount);
+            if (healedHp > 0)
+            {
+                Console.WriteLine($"{Name} 아이템 사용! HP +{healedHp} 회복됨!");
+                isUsed = true;
+            }
+            else
+            {
+                Console.WriteLine("HP가 이미 최대치 입니다.");
+            }
+        }
+        
+        // MP 회복
+        if (MpAmount > 0)
+        {
+            int healedMP = player.HealMp(MpAmount);
+            if (healedMP > 0)
+            {
+                Console.WriteLine($"{Name} 아이템 사용! MP +{healedMP} 회복됨!");
+                isUsed = true;
+            }
+            else
+            {
+                Console.WriteLine("MP가 이미 최대치 입니다.");
+            }
+        }
         
         return isUsed;
     }
